b2f1e2036e5850a2de39478ac3dc3551e33e57fa
squarebe <squarebe03@gmail.com>
2015-08-24 09:43:27 +0100

    first test passing

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -4,5 +4,10 @@ require_relative './models/link'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
   run! if app_file == $0
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,7 +1,8 @@
 feature 'Viewing links' do
 
   scenario 'I can see existing links on the links page' do
-    Link.new(url: 'htttp:??www.makersacademy.com', title: 'Makers Academy')
+    link = Link.new(url: 'htttp:??www.makersacademy.com', title: 'Makers Academy')
+    link.save  # added these lines to pass the test.
     visit '/links'
     expect(page.status_code).to eq 200
     within 'ul#links' do
